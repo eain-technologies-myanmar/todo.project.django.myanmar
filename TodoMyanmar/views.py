6b4bfe433904be63f50cfa2bfe412c394863a98d
Thet Win Htwe <thetwinhtwe@Thets-MacBook-Pro.local>
2020-12-18 03:21:34 +0630

    database မှ အချက်အလက်များကို စာမျက်နှာပေါ်သို့ ပေးပို့ ပြသမှု စစ်ဆေးခြင်း

--- TodoMyanmar/views.py
+++ TodoMyanmar/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse # HttpReponse ကိုသုံးချင်လို့ import လုပ်ပြီး သုံးပါတယ် 
+from .models import Task
 
 # Create your views here.
 def index(request):
 	#return HttpResponse('<h1>လုပ်ဆောင်</h1>') # ပေါ်ချင်တာ ရေးပြီးတဲ့အခါ လက်တွေလုပ်ဆောင်နိုင်ရန် urls.py တွင် ကြေညာ၍ အသုံးပြုရမယ်
-	return render(request,"index.html",{})
+	#return render(request,"index.html",{})
+	tasks=Task.objects.all()
+
+	context={'tasks':tasks}
+	return render(request,'index.html',context) # index page သို့ database မှအချက်အလက်များ ပို့ဆောင်
