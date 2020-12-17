1d71fdb16f362d9e34d27309b95770d340f85bdb
Thet Win Htwe <thetwinhtwe@Thets-MacBook-Pro.local>
2020-12-18 04:28:45 +0630

    လုပ်ဆောင်ချက်များ ရေးသားနိုင်ရန် ထည့်သွင်းခြင်း

--- TodoMyanmar/views.py
+++ TodoMyanmar/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse # HttpReponse ကိုသုံးချင်လို့ import လုပ်ပြီး သုံးပါတယ် 
 from .models import Task # models.py ထဲမှ Task ကို ယူသုံးပါမယ်
 from .forms import TaskForm # forms.py မှ TaskForm ကို ခေါ်သုံးပါမယ်
@@ -7,8 +7,12 @@ def index(request):
 	#return HttpResponse('<h1>လုပ်ဆောင်</h1>') # ပေါ်ချင်တာ ရေးပြီးတဲ့အခါ လက်တွေလုပ်ဆောင်နိုင်ရန် urls.py တွင် ကြေညာ၍ အသုံးပြုရမယ်
 	#return render(request,"index.html",{})
 	tasks=Task.objects.all()
-	form =TaskForm()
+	form =TaskForm() #forms.py က TaskForm ကို ​form ထဲထည့်
 
-
+	if request.method == 'POST': # data ထည့်တာ ရှိခဲ့ရင်
+		form = TaskForm(request.POST) 
+		if form.is_valid(): # မှန်ရင် save
+			form.save()
+		return redirect('/') # ပြီးမှ မူလစာမျက်နှာကို ပြန်ခေါ်
 	context={'tasks':tasks,'form':form}
 	return render(request,'index.html',context) # index page သို့ database မှအချက်အလက်များ ပို့ဆောင်
