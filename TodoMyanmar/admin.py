913ac96326fcdc5137a0db0fc0e7ade05c4ceb31
Thet Win Htwe <thetwinhtwe@Thets-MacBook-Pro.local>
2020-12-17 22:50:36 +0630

    admin panel တွင် ထည့်သွင်းခြင်း

--- TodoMyanmar/admin.py
+++ TodoMyanmar/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Task # models.py ထဲမှ class Task ကို ယူသုံးမယ် 
+
+admin.site.register(Task) # admin panel တွင် ပေါ်ရန် ရေးသားခြင်း
